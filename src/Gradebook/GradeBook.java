5b83b74f104e0178b705b80aacd2a165641f5f0a
Leonardo Montes <leonardo.montesm@gmail.com>
2023-04-11 21:08:12 -0500

    Add files from session7 POO basics, gradebood getters and setters

--- src/Gradebook/GradeBook.java
+++ src/Gradebook/GradeBook.java
@@ -2,8 +2,40 @@ package Gradebook;
 
 public class GradeBook {
 	
+	
+	private String nombreCurso;
+	
+	
+	
+	
+	
+	
+	/**
+	 * @return the nombreCurso
+	 */
+	public String getNombreCurso() {
+		return nombreCurso;
+	}
+
+
+
+
+
+
+	/**
+	 * @param nombreCurso the nombreCurso to set
+	 */
+	public void setNombreCurso(String nombreCurso) {
+		this.nombreCurso = nombreCurso;
+	}
+
+
+
+
+
+
 	public void displayMessage() {
-		System.out.println("Bienvenido al sistema de Notas");
+		System.out.println("Bienvenido al sistema de Notas: " + getNombreCurso());
 	}
 
 }
--- src/Gradebook/GradeBookTest.java
+++ src/Gradebook/GradeBookTest.java
@@ -1,12 +1,22 @@
 package Gradebook;
 
+import java.util.Scanner;
+
 public class GradeBookTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GradeBook myGradeBook = new GradeBook();
 		
-		myGradeBook.displayMessage();
+		Scanner lector = new Scanner(System.in);
+		
+		GradeBook planillaNotas = new GradeBook();
+		
+		System.out.println("Escriba el nombre del curso");
+		String nombreCurso = lector.nextLine();
+		
+		planillaNotas.setNombreCurso(nombreCurso);
+		
+		planillaNotas.displayMessage();
 	}
 
 }
