2b81f1784969ed6eabd2fbc5725db93109e278cd
Leonardo Montes <leonardo.montesm@gmail.com>
2023-04-25 20:39:34 -0500

    Add new constructors from new attributes, Encapasulation 2 part

--- src/Gradebook/GradeBook.java
+++ src/Gradebook/GradeBook.java
@@ -4,23 +4,37 @@ public class GradeBook {
 	
 	
 	private String nombreCurso;
+	private int aforo;
 	
 	
-	
-	
-	
-	
+	/**
+	 * 
+	 */
+	public GradeBook() {
+		}
+
+	/**
+	 * @param nombreCurso
+	 * @param aforo
+	 */
+	public GradeBook(String nombreCurso, int aforo) {
+		this.nombreCurso = nombreCurso;
+		this.aforo = aforo;
+	}
+
+	/**
+	 * @param nombreCurso
+	 */
+	public GradeBook(String nombreCurso) {
+		this.nombreCurso = nombreCurso;
+	}
+
 	/**
 	 * @return the nombreCurso
 	 */
 	public String getNombreCurso() {
 		return nombreCurso;
 	}
-
-
-
-
-
 
 	/**
 	 * @param nombreCurso the nombreCurso to set
@@ -29,13 +43,20 @@ public class GradeBook {
 		this.nombreCurso = nombreCurso;
 	}
 
+	public int getAforo() {
+		return aforo;
+	}
 
-
-
-
+	public void setAforo(int aforo) {
+		this.aforo = aforo;
+	}
 
 	public void displayMessage() {
 		System.out.println("Bienvenido al sistema de Notas: " + getNombreCurso());
+	}
+	
+	public void determineClassAverage() {
+		
 	}
 
 }
--- src/Gradebook/GradeBookTest.java
+++ src/Gradebook/GradeBookTest.java
@@ -9,14 +9,26 @@ public class GradeBookTest {
 		
 		Scanner lector = new Scanner(System.in);
 		
-		GradeBook planillaNotas = new GradeBook();
+		GradeBook planillaNotasCurso1 = new GradeBook();
+	
+		GradeBook planillaNotasCurso2 = new GradeBook("Curso2");
+		
+		GradeBook planillaNotasCurso3 = new GradeBook("Curso3", 20);
+				
+		
 		
 		System.out.println("Escriba el nombre del curso");
 		String nombreCurso = lector.nextLine();
 		
-		planillaNotas.setNombreCurso(nombreCurso);
+		planillaNotasCurso1.setNombreCurso(nombreCurso);
 		
-		planillaNotas.displayMessage();
+		planillaNotasCurso1.displayMessage();
+		
+		planillaNotasCurso2.displayMessage();
+		
+		planillaNotasCurso3.displayMessage();
+		
+		System.out.println("Numero de estudiantes del curso: "+ planillaNotasCurso3.getNombreCurso() + " Aforo: " +planillaNotasCurso3.getAforo());
 	}
 
 }
