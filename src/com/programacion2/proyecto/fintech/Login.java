f00f75185eec243131a06ae6ca269b66b16a64bc
Leonardo Montes <leonardo.montesm@gmail.com>
2023-05-09 20:15:37 -0500

    Add first GUI Swing demo

---
+++ src/com/programacion2/proyecto/fintech/Login.java
@@ -0,0 +1,49 @@
+package com.programacion2.proyecto.fintech;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("New button");
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Bienvenido al sistema de Fintech");
+		contentPane.add(lblNewLabel);
+	}
+
+}
